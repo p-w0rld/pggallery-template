88c6eb104e28718edb9ee218c57cc9b40b4a1fb2
p-w0rld <39331476+p-w0rld@users.noreply.github.com>
2024-11-03 19:59:01 -0600

    Initial commit

---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "name": "pgGallery",
+    "email": "contactß@pggallery.com",
+    "phoneForTel": "",
+    "phoneFormatted": "",
+    "address": {
+        "lineOne": "",
+        "lineTwo": "",
+        "city": "Kansas City",
+        "state": "MO",
+        "zip": "64106",
+        "country": "US",
+        "mapLink": ""
+    },
+    "socials": {
+        "facebook": "",
+        "instagram": "",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.pggallery.com"
+};
